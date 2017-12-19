ff767b1eff5619b10c53f2f6f4384aa2896e9756
DioSatyaloka <dio.satyaloka@yahoo.com>
2017-12-19 16:06:23 +0700

    Initial commit

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    //public GameObject[] platforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObjectPooler[] objectPools;
+
+    private float minHeight;
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange;
+    private float heightChange;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+
+        platformWidths = new float[objectPools.Length];
+
+        for(int i = 0; i < objectPools.Length; i++)
+        {
+            platformWidths[i] = objectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            platformSelector = Random.Range(0, objectPools.Length);
+
+            heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+            if(heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            } 
+            else if(heightChange < minHeight)
+            {
+                heightChange = minHeight;
+            }
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            //Instantiate(/*platform*/ platforms[platformSelector], transform.position, transform.rotation);
+                
+            GameObject newPlatform = objectPools[platformSelector].GetPooledObejct();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D playerRB;
+
+    public float moveSpeed = 0f;
+    public float jumpForce = 0f;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        playerRB = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+        myAnimator = GetComponent<Animator>();
+
+        jumpTimeCounter = jumpTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        playerRB.velocity = new Vector2(moveSpeed, playerRB.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            if(grounded)
+                playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+            // playerRB.AddRelativeForce(transform.up * jumpForce, ForceMode2D.Force);
+
+        }
+
+        if (Input.GetKey(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary))
+        {
+            if(jumpTimeCounter > 0)
+            {
+                playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        {
+            jumpTimeCounter = 0;
+        }
+
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
+        myAnimator.SetFloat("Speed", playerRB.velocity.x);
+
+        myAnimator.SetBool("Grounded", grounded);
+
+    }
+}
